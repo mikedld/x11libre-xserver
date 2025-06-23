97582ac0ce4a6ba11cbb7a19c42f41fd124f1b1d
Mike Gelfand <mikedld@mikedld.com>
2025-06-23 22:46:15 +0100

    xkb: move include guards up to wrap the whole file
    See: https://gcc.gnu.org/onlinedocs/cppinternals/Guard-Macros.html
    Signed-off-by: Mike Gelfand <mikedld@mikedld.com>

--- xkb/xkbDflts.h
+++ xkb/xkbDflts.h
@@ -1,11 +1,12 @@
 /* This file generated automatically by xkbcomp */
 /* DO  NOT EDIT */
-#ifdef HAVE_DIX_CONFIG_H
-#include <dix-config.h>
-#endif
 
 #ifndef DEFAULT_H
 #define DEFAULT_H 1
+
+#ifdef HAVE_DIX_CONFIG_H
+#include <dix-config.h>
+#endif
 
 #define GET_ATOM(d,s)	MakeAtom(s,strlen(s),1)
 #define DPYTYPE	char *
